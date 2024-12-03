23b130919df03bc5ccd4f8e2eb728a8d5e3d35ce
Andrew <andrglazkovandr@gmail.com>
2024-12-03 10:28:30 +0300

    Laba6

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,19 @@
-package main
-
-// здесь надо написать код
-
-func main() {
-	// и здесь тоже
+package main 
+import (
+	"fmt" 
+	"net/http"
+)
+// Обработчик HTTP-запросов
+func handler(w http.ResponseWriter, r *http.Request) { 
+	w.Write([]byte("Hello world!"))
 }
+func main() {
+// Регистрируем обработчик для пути "/" 
+http.HandleFunc("/", handler)
+// Запускаем веб-сервер на порту 8080 
+err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
+}
+	
